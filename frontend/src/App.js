c53143ba611515d9b05f401380322f1d65c1ab17
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-19 15:55:41 +0530

    Built Home page & added styles to various components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import WebFont from 'webfontloader';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import Home from './components/layout/Home/Home';
 
 function App() {
 
@@ -18,7 +19,9 @@ function App() {
   return (
     <Router>
       <Header />
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Footer />
     </Router>
   );
