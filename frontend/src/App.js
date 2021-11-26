6e41e0b49402772bc08f40aa451e6ba56d6c6652
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-26 09:38:14 +0530

    Created an update password component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import WebFont from 'webfontloader';
-import Header from './components/layout/Header/Header';
-import Footer from './components/layout/Footer/Footer';
-import Home from './components/Home/Home';
-import ProductDetails from './components/Product/ProductDetails';
-import Products from './components/Product/Products';
-import Search from './components/Product/Search';
-import LoginSignup from './components/User/LoginSignup';
-import store from './store';
-import { loadUser } from './actions/userAction';
-import { useSelector } from 'react-redux';
-import UserOptions from './components/layout/Header/UserOptions';
-import ProtectedRoute from './components/Route/ProtectedRoute';
-import Profile from './components/User/Profile';
-import UpdateProfile from './components/User/UpdateProfile';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WebFont from "webfontloader";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
+import Home from "./components/Home/Home";
+import ProductDetails from "./components/Product/ProductDetails";
+import Products from "./components/Product/Products";
+import Search from "./components/Product/Search";
+import LoginSignup from "./components/User/LoginSignup";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import { useSelector } from "react-redux";
+import UserOptions from "./components/layout/Header/UserOptions";
+import ProtectedRoute from "./components/Route/ProtectedRoute";
+import Profile from "./components/User/Profile";
+import UpdateProfile from "./components/User/UpdateProfile";
+import UpdatePassword from "./components/User/UpdatePassword";
 
 function App() {
-
-  const { isAuthenticated, user } = useSelector(state => state.user);
+  const { isAuthenticated, user } = useSelector((state) => state.user);
 
   React.useEffect(() => {
     WebFont.load({
       google: {
-        families: ['Roboto', 'Droid Sans', 'Chilanka']
-      }
-    })
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
 
     store.dispatch(loadUser());
   }, []);
@@ -34,16 +34,29 @@ function App() {
   return (
     <Router>
       <Header />
-      { isAuthenticated && <UserOptions user={user} /> }
+      {isAuthenticated && <UserOptions user={user} />}
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/product/:id' element={<ProductDetails />} />
-        <Route exact path='/products' element={<Products />} />
-        <Route exact path='/products/:keyword' element={<Products />} />
-        <Route exact path='/search' element={<Search />} />
-        <Route exact path='/login' element={<LoginSignup />}/>
-        <Route exact path='/account' element={<ProtectedRoute component={Profile}/>}/>
-        <Route exact path='/update-profile' element={<ProtectedRoute component={UpdateProfile}/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/product/:id" element={<ProductDetails />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/products/:keyword" element={<Products />} />
+        <Route exact path="/search" element={<Search />} />
+        <Route exact path="/login" element={<LoginSignup />} />
+        <Route
+          exact
+          path="/account"
+          element={<ProtectedRoute component={Profile} />}
+        />
+        <Route
+          exact
+          path="/update-profile"
+          element={<ProtectedRoute component={UpdateProfile} />}
+        />
+        <Route
+          exact
+          path="/update-password"
+          element={<ProtectedRoute component={UpdatePassword} />}
+        />
       </Routes>
       <Footer />
     </Router>
--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, CLEAR_ERRORS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL } from "../constants/userConstants";
+import { LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, CLEAR_ERRORS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAIL } from "../constants/userConstants";
 
 export const loginUser = (email, password) => async (dispatch) => {
     try {
@@ -53,10 +53,22 @@ export const updateProfile = (userData) => async (dispatch) => {
         dispatch({ type: UPDATE_PROFILE_REQUEST });
         const config = { headers: { 'Content-Type': 'multipart/form-data'} };
         const { data } = await axios.put(`api/v1/me/profile`, userData, config);
-        console.log(data);
+
         dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
     } catch (error) {
         dispatch({ type: UPDATE_PROFILE_FAIL, payload: error.response.data.message })
+    }
+}
+
+export const updatePassword = (passwords) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_PASSWORD_REQUEST });
+        const config = { headers: { 'Content-Type': 'application/json'} };
+        const { data } = await axios.put(`api/v1/password/update`, passwords, config);
+
+        dispatch({ type: UPDATE_PASSWORD_SUCCESS, payload: data.success });
+    } catch (error) {
+        dispatch({ type: UPDATE_PASSWORD_FAIL, payload: error.response.data.message })
     }
 }
 
