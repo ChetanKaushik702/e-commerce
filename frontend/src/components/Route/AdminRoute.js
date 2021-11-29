a07fe2cf1a0d16f6f72f38e0516c5bbacf0dac82
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-29 13:01:43 +0530

    Created an admin protected route component

---
+++ frontend/src/components/Route/AdminRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate } from 'react-router';
+import { useSelector } from 'react-redux';
+
+const AdminRoute = ({ component: Component }) => {
+    const { loading, user, isAuthenticated } = useSelector(state => state.user);
+    if (loading === undefined || loading === true)
+        return <></>;
+    if (isAuthenticated === false || (user.role !== 'admin')) 
+        <Navigate to='/login'/>;
+    
+    return (
+        <Component />
+    )
+}
+
+export default AdminRoute;
